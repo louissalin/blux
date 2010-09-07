e3e5a63f2545274f138b3f3d1154d9039d9ff678
Louis Salin <louis.phil@gmail.com>
2010-09-07 16:45:12 -0500

    added a few tests

--- lib/blog_manager.rb
+++ lib/blog_manager.rb
@@ -1,7 +1,37 @@
 class BlogManager
-	attr_accessor :home
+	attr_accessor :home, :blux_dir, :blux_rc
+	attr_accessor :launch_editor_cmd
 
-	def initialize
+	def initialize(io = STDOUT)
 		@home = ENV['HOME']
+		@blux_dir = "#{@home}/.blux"
+		@blux_rc = "#{@home}/.bluxrc"
+
+		@io = io
+	end
+
+	def start
+		dir_name = "#{@home}/.blux"
+		Dir.mkdir(dir_name) unless Dir.exists?(dir_name)
+
+		draft_dir = "#{dir_name}/drafts"
+		Dir.mkdir(draft_dir) unless Dir.exists?(draft_dir)
+	end
+
+	def load_config
+		system "touch #{@blux_rc}" unless File.exists? @blux_rc
+
+		editor_line = `grep editor: #{@blux_rc}`
+		editor_match = editor_line =~ /^editor:\s(.+)$/
+		@launch_editor_cmd = $1
+
+		validate
+	end
+
+private
+	def validate
+		if (@launch_editor_cmd == nil)
+			@io << 'please specify an editor in .bluxrc: editor: [your editor of choice]'
+		end
 	end
 end
--- spec/blog_manager_spec.rb
+++ spec/blog_manager_spec.rb
@@ -3,14 +3,82 @@ require 'blog_manager.rb'
 describe BlogManager do
 	before :each do
 		ENV['HOME'] = File.dirname(__FILE__)
-		@manager = BlogManager.new
+		@blux_rc = "#{File.dirname(__FILE__)}/.bluxrc"
+		@blux= "#{File.dirname(__FILE__)}/.blux"
+
+		@io = mock("IO")
+		def @io.<<(str) end
+		@manager = BlogManager.new(@io)
 	end
 
-	it "should read the HOME variable from the environment" do
-		@manager.home.should == File.dirname(__FILE__)
+	after :each do
+		system "rm -Rf #{@blux}"
+		system "rm #{@blux_rc}" if File.exists? @blux_rc
 	end
 
-	it "should clear create a .blux folder in the home dir if it doesn't exist" do
-		File.exists?("#{File.dirname(__FILE__)}/.blux").should == true
+	context "loading with no config file" do
+		before :each do
+			@manager.load_config
+		end
+
+		it "should create an empty config file it doesn't exist" do
+			File.exists?(@blux_rc).should == true
+		end
 	end
+
+	context "loading the editor from the config file when it doesn't exist" do
+		it "should show a warning" do
+			@io.should_receive(:<<).with('please specify an editor in .bluxrc: editor: [your editor of choice]')
+			@manager.load_config
+		end
+	end
+
+	context "loading the editor from the config file" do
+		before :each do
+			create_config
+			@manager.load_config
+		end
+
+		it "should read the editor from the config file" do
+			@manager.launch_editor_cmd.should == 'gedit'
+		end
+
+		def create_config
+			config = "editor: gedit"
+			system "echo #{config} > #{@blux_rc}"
+		end
+	end
+
+	context "starting the blog manager" do
+		before :each do
+			@manager.start
+		end
+
+		it "should read the HOME variable from the environment" do
+			@manager.home.should == File.dirname(__FILE__)
+		end
+		
+		it "should compute the blux dir" do
+			@manager.blux_dir.should == @blux
+		end
+
+		it "should compute the bluxrc file" do
+			@manager.blux_rc.should == @blux_rc
+		end
+
+		it "should clear create a .blux folder in the home dir if it doesn't exist" do
+			File.exists?(@blux).should == true
+		end
+
+		it "should clear create a draft folder in the .blux dir if it doesn't exist" do
+			File.exists?("#{@blux}/drafts").should == true
+		end
+	end
+
+	#context "creating a draft" do
+		#before :each do
+			#@manager.start
+			#@manager.create_new_draft
+		#end
+	#end
 end
