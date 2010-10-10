ce08e7952dad75a6f3a097ab6cbe87b9896ef058
Louis Salin <louis.phil@gmail.com>
2010-10-10 17:00:23 -0500

    GPLed the code

--- lib/blux_config_reader.rb
+++ lib/blux_config_reader.rb
@@ -1,3 +1,21 @@
+#
+# Copyright 2010 Louis-Philippe Salin de l'Etoile, aka Louis Salin
+# email: louis.phil@gmail.com
+#
+# This file is part of Blux.
+#
+# Blux is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Blux is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Blux.  If not, see <http://www.gnu.org/licenses/>.
 class BluxConfigurationReader
 	attr_reader :launch_editor_cmd, :html_converter_cmd
 	attr_reader :blog, :author_name, :user_name, :password
