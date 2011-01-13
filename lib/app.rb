aa54ce371100464b27b2f010db788e2db5cb207f
Louis Salin <louis.phil@gmail.com>
2011-01-13 13:06:30 -0600

    now launching the editor when creating a new post. Also added copyright info to app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,33 @@
+#!/usr/bin/env ruby
+#
+# Copyright 2011 Louis-Philippe Salin de l'Etoile, aka Louis Salin
+# email: louis.phil@gmail.com
+#
+# This file is part of Blux.
+#
+# Blux is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Blux is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Blux.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'config'
 
 module Blux
 	class App
 		def initialize
 			ConfigReader.new.read_config
+		end
+
+		def create_new_post
+			system Config.instance.editor_cmd
 		end
 	end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,22 +1,13 @@
 require 'app'
 
-describe Blux::App, "initialize" do
-	it "should load the config file" do
-		stub_config_reader
-
-		Blux::App.new
-		Blux::Config.instance.editor_cmd.should eq('vi')
-
-		restore_config_reader
-	end
-
+module AppSpecHelper
 	def stub_config_reader
 		Blux::ConfigReader.class_eval do
 			alias :old_read_config :read_config
 
 			def read_config
 				config = Blux::Config.instance
-				config.editor_cmd = 'vi'
+				config.editor_cmd = 'some_editor_cmd'
 			end
 		end
 	end
@@ -25,5 +16,31 @@ describe Blux::App, "initialize" do
 		Blux::ConfigReader.class_eval do
 			alias :read_config :old_read_config
 		end
+	end
+end
+
+describe Blux::App, "initialize" do
+	include AppSpecHelper
+
+	it "should load the config file" do
+		stub_config_reader
+
+		Blux::App.new
+		Blux::Config.instance.editor_cmd.should eq('some_editor_cmd')
+
+		restore_config_reader
+	end
+end
+
+describe Blux::App, "when creating a new post" do
+	include AppSpecHelper
+	it "should launch the editor defined in the config" do
+		stub_config_reader
+
+		app = Blux::App.new
+		app.should_receive(:system).with('some_editor_cmd')
+		app.create_new_post
+
+		restore_config_reader
 	end
 end
